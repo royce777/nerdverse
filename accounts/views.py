afa46e0096d361ce944ecc400d1d681d2d14efef
r0yce <marioboom10@gmail.com>
2021-02-23 11:32:38 +0100

    requiring login + model forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def signup_view(request):
@@ -21,7 +21,14 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
             return redirect('articles:list')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
--- articles/urls.py
+++ articles/urls.py
@@ -21,5 +21,6 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.articles_list, name="list"),
+    path('create/', views.article_create, name ="create"),
     path('<slug:slug>' , views.article_detail, name="detail"),
 ]
